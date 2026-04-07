85e6517fa9bfc27f2267a571ec7e45a3091835c4
vashukumar001 <vashukumar1667@gmail.com>
2026-04-07 11:24:50 +0530

    Update Main.java

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
         // TODO: Read n integers into an array
         //       Implement BUBBLE SORT manually (no Arrays.sort)
@@ -17,6 +16,30 @@ public class Main {
         // Output:
         // 12 22 25 34 64
         // Swaps: 7
+        if (!sc.hasNextInt()) return;
+        int n = sc.nextInt();
+        int[] arr = new int[n];
 
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int swaps = 0;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swaps++;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + (i == n - 1 ? "" : " "));
+        }
+        System.out.println();
+        System.out.println("Swaps: " + swaps);
     }
 }
