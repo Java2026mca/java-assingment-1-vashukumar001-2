0c799cf39464399b620eb762b829f9e34545136e
vashukumar001 <vashukumar1667@gmail.com>
2026-04-07 11:25:56 +0530

    Update Main.java

--- src/q08/Main.java
+++ src/q08/Main.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
         // TODO: Implement a stack using an array (no Java Stack class)
         //       Process n operations:
@@ -26,6 +25,41 @@ public class Main {
         // 20
         // 10
         // EMPTY
+        if (!sc.hasNextInt()) return;
+        int n = sc.nextInt();
 
+        int[] stack = new int[n];
+        int top = -1;
+
+        for (int i = 0; i < n; i++) {
+            String op = sc.next().toUpperCase();
+
+            switch (op) {
+                case "PUSH":
+                    int x = sc.nextInt();
+                    stack[++top] = x;
+                    break;
+
+                case "POP":
+                    if (top == -1) {
+                        System.out.println("EMPTY");
+                    } else {
+                        System.out.println(stack[top--]);
+                    }
+                    break;
+
+                case "PEEK":
+                    if (top == -1) {
+                        System.out.println("EMPTY");
+                    } else {
+                        System.out.println(stack[top]);
+                    }
+                    break;
+
+                case "SIZE":
+                    System.out.println(top + 1);
+                    break;
+            }
+        }
     }
 }
