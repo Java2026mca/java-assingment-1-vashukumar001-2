307db4d98120aa039f0005806d144aab8b4e741f
vashukumar001 <vashukumar1667@gmail.com>
2026-04-07 12:06:01 +0530

    Update Main.java

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -2,26 +2,19 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
-        // TODO: Fill an N×N matrix in clockwise spiral order starting from 1
-        //       Print each row with values separated by single space
-        //       Then print: "Diagonal: X" where X = sum of primary diagonal (top-left to bottom-right)
-        //
-        // Input: 3
-        // Output:
-        // 1 2 3
-        // 8 9 4
-        // 7 6 5
-        // Diagonal: 15
         if (!sc.hasNextInt()) return;
         int n = sc.nextInt();
 
         int[][] matrix = new int[n][n];
+
         int val = 1;
         int top = 0, bottom = n - 1, left = 0, right = n - 1;
 
-        while (val <= n * n) {
+        while (top <= bottom && left <= right) {
+
             for (int i = left; i <= right; i++) {
                 matrix[top][i] = val++;
             }
@@ -32,28 +25,34 @@ public class Main {
             }
             right--;
 
-            for (int i = right; i >= left; i--) {
-                matrix[bottom][i] = val++;
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    matrix[bottom][i] = val++;
+                }
+                bottom--;
             }
-            bottom--;
 
-            for (int i = bottom; i >= top; i--) {
-                matrix[i][left] = val++;
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    matrix[i][left] = val++;
+                }
+                left++;
             }
-            left++;
         }
 
-        int diagonalSum = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print(matrix[i][j] + (j == n - 1 ? "" : " "));
-                if (i == j) {
-                    diagonalSum += matrix[i][j];
-                }
+                System.out.print(matrix[i][j]);
+                if (j != n - 1) System.out.print(" ");
             }
             System.out.println();
         }
 
-        System.out.println("Diagonal: " + diagonalSum);
+        int diagonalSum = 0;
+        for (int i = 0; i < n; i++) {
+            diagonalSum += matrix[i][i];
+        }
+
+        System.out.println("Diagonal1: " + diagonalSum);
     }
 }
