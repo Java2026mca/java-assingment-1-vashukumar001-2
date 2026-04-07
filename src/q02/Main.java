10795ac571875cc72c8a104ee3817547f15c58c4
vashukumar001 <vashukumar1667@gmail.com>
2026-04-07 11:17:52 +0530

    Update Main.java

--- src/q02/Main.java
+++ src/q02/Main.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
         // TODO: Print Pascal's Triangle for n rows
         //       Values on each row separated by single space
@@ -16,6 +15,27 @@ public class Main {
         // 1 2 1
         // 1 3 3 1
         // 1 4 6 4 1
+        if (!sc.hasNextInt()) return;
+        int n = sc.nextInt();
 
+        int[][] triangle = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    triangle[i][j] = 1;
+                } else {
+                    triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+                }
+                
+                System.out.print(triangle[i][j]);
+                
+                if (j < i) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
+  
